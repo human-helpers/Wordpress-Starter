348cda0477fe3f0dd740cb3c53dd9c8c2a032d98
Eric Mikkelsen <eric.mikkelsen@boldium.com>
2021-09-13 15:31:54 -0700

    feat: adds search & copyright to footer

--- template-parts/organisms/footer.php
+++ template-parts/organisms/footer.php
@@ -1,3 +1,11 @@
-<footer>
-    
-</footer>
+<?php
+$search_form = get_search_form();
+$copyright = sprintf(__("Copyright %s", "boldium"), date("Y"));
+echo "
+    <footer>
+        $search_form
+        <small>
+            © $copyright
+        </small>
+    </footer>
+";
