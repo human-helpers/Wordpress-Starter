054ce6990aa5b812a546ae4d9c4701ca915c91ae
Eric Mikkelsen <eric.mikkelsen@boldium.com>
2021-09-13 14:36:29 -0700

    feat: adds article to singular.php

--- singular.php
+++ singular.php
@@ -7,4 +7,7 @@ get_template_part("template-parts/organisms/main-menu");
 get_template_part("template-parts/organisms/page-header", get_post_type(), [
     "heading" => wp_title("", false),
 ]);
+echo "<main>";
+get_template_part("template-parts/molecules/article", get_post_type());
+echo "</main>";
 get_footer();
