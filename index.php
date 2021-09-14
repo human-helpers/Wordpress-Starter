a2d6ca6572bdfedf1283031cec4d85e8a6ad613e
Eric Mikkelsen <dev@boldium.com>
2021-09-14 13:45:26 -0700

    feat: moves <header> into <main> so h1 is in main

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ get_header();
 
 get_template_part("template-parts/organisms/main-menu");
 
+echo "<main>";
 get_template_part("template-parts/organisms/page-header", "archive", [
     "heading" => wp_title("", false) ?: get_option("blogname"),
 ]);
-echo "<main>";
 get_template_part("template-parts/molecules/article-list", "archive");
 echo "</main>";
 get_footer();
--- singular.php
+++ singular.php
@@ -4,10 +4,10 @@ get_header();
 
 get_template_part("template-parts/organisms/main-menu");
 
+echo "<main>";
 get_template_part("template-parts/organisms/page-header", get_post_type(), [
     "heading" => wp_title("", false),
 ]);
-echo "<main>";
 get_template_part("template-parts/molecules/article", get_post_type());
 echo "</main>";
 get_footer();
