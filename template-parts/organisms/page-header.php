9ab888d21b5a54438c340a9e72554856e5f52923
Eric Mikkelsen <dev@boldium.com>
2021-09-14 13:47:07 -0700

    feat: updates page-header to use heading template-part to get ready for skip link

--- template-parts/organisms/page-header.php
+++ template-parts/organisms/page-header.php
@@ -10,11 +10,14 @@ $defaults = [
 extract(array_merge($defaults, $args));
 
 $body = $body ? "<div class='page-header__body'>$body</div>" : "";
-echo "
-    <$is class='page-header $class'>
-        <h$heading_level class='page-header__heading'>
-            $heading
-        </h$heading_level>
-        $body
-    </$is>
-";
+
+echo "<$is class='page-header $class'>";
+
+get_template_part("template-parts/atoms/heading", "", [
+    "text" => $heading,
+    "heading_level" => $heading_level,
+    "class" => "page-header__heading",
+]);
+
+echo "$body 
+</$is>";
